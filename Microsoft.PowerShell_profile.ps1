e93c3f18199fd1948474655e989d6f23f4b4d53e
Pedro Scotti <129436347+PeppoScotti@users.noreply.github.com>
2025-10-04 11:23:26 -0300

    Eliminé Alias que interfieren con mis funciones

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,17 @@
+# --- Prompt (oh-my-posh) ---
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/PeppoScotti/Terminal-Theme/refs/heads/main/onehalf.minimal.oneline.omp.json' | Invoke-Expression
 
-# Git: navegación rápida
+# --- Util: eliminar alias integrado si existe ---
+function Remove-AliasIfExists {
+    param([Parameter(Mandatory)][string]$Name)
+    if (Test-Path "alias:$Name") { Remove-Item "alias:$Name" -Force }
+}
+
+# --- Quitar alias que chocan con mis funciones ---
+Remove-AliasIfExists gps   # integrado: Get-Process
+Remove-AliasIfExists gcb   # integrado: Get-Clipboard
+
+# --- Git ---
 function gst { git status @Args }
 function gco { git commit -m @Args }
 function gch { git checkout @Args }
@@ -14,24 +25,20 @@ function gbr { git branch @Args }
 function gmg { git merge @Args }
 function gcl { git clone @Args }
 
-# Navegación de carpetas
+# --- Navegación de carpetas ---
 function ..   { Set-Location .. }
 function ...  { Set-Location ../.. }
 function .... { Set-Location ../../.. }
 
-# Atajos de directorios comunes
+# --- Atajos de directorios comunes ---
 function docs { Set-Location ~/Documents }
-function dsk  { Set-Location ~/Desktop }
+function desk  { Set-Location ~/Desktop }
 function dl   { Set-Location ~/Downloads }
 function pr   { Set-Location D:\Programacion}
 function tr   { Set-Location D:\Programacion\trescode}
 
-
-# Limpieza de pantalla
-function cls  { Clear-Host }
-
-# Listado de archivos con detalles
+# --- Listado de archivos con detalles ---
 function ll   { Get-ChildItem -Force @Args }
 
-# Chris Titus Windows Utility
+# --- Chris Titus Windows Utility ---
 function wu { irm "https://christitus.com/win" | iex }
